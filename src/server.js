809e7d71eb6ab6eb4b65b4f6fe437960e7c25997
Adilio Figueiredo <adiliosouza@me.com>
2025-07-02 21:41:41 -0300

    mais uma alteração para validar o CI

--- src/server.js
+++ src/server.js
@@ -18,5 +18,5 @@ server.use(/^(?!\/(public|livros|autores|categorias)).*$/, authenticationMiddlew
 server.use(router) //rotas na prota 3000
 
 server.listen(8000, () => {
-  console.log("API disponível nesse link http://localhost:8000")
+  console.log("Acessa a API em http://localhost:8000")
 })
